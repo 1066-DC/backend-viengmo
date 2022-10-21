a3b9824aba9cdcd7e11891e1c71674da03f864af
syldor <dorey.consulting@gmail.com>
2022-10-21 14:59:47 +0700

    init strapi

---
+++ config/database.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', env('DB_HOST')),
+      port: env.int('DATABASE_PORT', env('DB_PORT')),
+      database: env('DATABASE_NAME', env('DB_NAME')),
+      user: env('DATABASE_USERNAME', env('DB_USER')),
+      password: env('DATABASE_PASSWORD', env('DB_PASSWORD')),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+  },
+});
